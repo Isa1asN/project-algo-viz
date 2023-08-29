f2bdc6e07149c0c5daebce65ec4f7ff817ce55d7
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-08-29 12:42:39 +0300

    navbar complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
 import {useParams} from './context/context'
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   console.log(useParams())
   return(
-    <div>BFS</div>
+    <Navbar />
   );
  };
 
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,60 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react'
+import './navbar.css'
+import { useParams } from '../../context/context'
+
+
+export default function Navbar() {
+
+  // const [algo,setalgo] = useState('')
+  const {mode,setmode,algo,setalgo,setres,setrun}=useParams()
+
+  
+
+  return (
+    <div className='navbar'>
+      <div className='container'>
+       <button type="button" className={['btn' ,'btn-primary', mode=='setstart'? 'selected' : ''].join(' ')} onClick={()=>{
+        if(mode == 'setstart') setmode(null)
+        else {setmode('setstart')}
+       }}>
+        <p>S</p>
+       </button>
+       <button type="button" className={['btn' ,'btn-primary', mode=='settarget'? 'selected' : ''].join(' ')} onClick={()=>{
+        if(mode == 'settarget') setmode(null)
+        else {setmode('settarget')}
+       }}>
+       <p>E</p>
+       </button>
+       <button type="button" className={['btn' ,'btn-primary', mode=='addbricks'? 'selected' : ''].join(' ')} onClick={()=>{
+        if(mode == 'addbricks') setmode(null)
+        else {setmode('addbricks')}
+       }}>
+       <i className="bi bi-bricks"></i>
+       </button>
+       <button type="button" className={['btn' ,'btn-primary', mode=='addweight'? 'selected' : ''].join(' ')} onClick={()=>{
+        if(mode == 'addweight') setmode(null)
+        else {setmode('addweight')}
+       }}>
+       <i className="bi bi-virus"></i>
+       </button>
+       <button type="button" className="btn btn-primary" onClick={()=>{setres((old)=>{ return !old})}}>
+       <i className="bi bi-arrow-counterclockwise"></i> 
+       </button>
+       <button type="button" className="btn btn-primary" onClick={()=>{setrun((old)=>{return !old})}}>
+       <i className="bi bi-caret-right"></i> 
+       </button>
+       <div>
+       <select className="form-select" aria-label="Default select example"  value={algo} onChange={(e)=>{
+        setalgo(e.target.value)
+       }}>
+       <option value=''>Choose your algorithm</option>
+       <option value="dijkstra">dijkstra</option>
+       <option value="BDS">BDS</option>
+       <option value="BFS">BFS</option>
+</select>
+       </div>
+      </div>
+    </div>
+  )
+}
