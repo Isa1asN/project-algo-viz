02a5afd70e3a4135cf86dc1b2734d2ad550980fc
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-08-29 11:49:23 +0300

    basic state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
+import {useParams} from './context/context'
 
 const App = () => {
+  console.log(useParams())
   return(
     <div>BFS</div>
   );
  };
+
 
 export default App;
