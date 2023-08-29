97c3988202580a0cdcdb3d30f32a08a95ec7fc46
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-08-29 13:55:37 +0300

    basic project complete

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useRef } from 'react'
@@ -22,6 +23,139 @@ export default function Grid() {
    return array
  }
 
+  function BFS(graph,hashmap,prevmap,start,target){
+    let queue=[start]
+    let count=0
+    hashmap[`${start.x}-${start.y}`]=true
+    while (queue.length > 0){
+      count+=1
+      let c=queue.pop()
+      refarray[c.x+c.y*50].current.style['transition-delay']=`${count * 8}ms`
+      refarray[c.x+c.y*50].current.classList.add('visited')
+      if (c.x == target.x && c.y == target.y) return [c,count]
+ 
+      if(c.x+1 < 50 && !hashmap[`${c.x+1}-${c.y}`] && !graph[c.y][c.x+1].iswall){
+        queue.unshift({x:c.x +1,y:c.y})
+        prevmap[`${c.x+1}-${c.y}`]={...c}
+        hashmap[`${c.x+1}-${c.y}`]=true
+      }
+      if(c.x-1 >=0 && !hashmap[`${c.x-1}-${c.y}`] && !graph[c.y][c.x-1].iswall){
+        queue.unshift({x:c.x -1,y:c.y})
+        prevmap[`${c.x-1}-${c.y}`]={...c}
+        hashmap[`${c.x-1}-${c.y}`]=true
+      }
+      if(c.y+1 < 25 && !hashmap[`${c.x}-${c.y+1}`] && !graph[c.y+1][c.x].iswall){
+        queue.unshift({x:c.x ,y:c.y+1})
+        prevmap[`${c.x}-${c.y+1}`]={...c}
+        hashmap[`${c.x}-${c.y+1}`]=true
+      }
+      if(c.y-1 >=0 && !hashmap[`${c.x}-${c.y-1}`] && !graph[c.y-1][c.x].iswall){
+        queue.unshift({x:c.x ,y:c.y-1})
+        prevmap[`${c.x}-${c.y-1}`]={...c}
+        hashmap[`${c.x}-${c.y-1}`]=true
+      }
+    }
+    return null
+  }
+
+  function BDS(graph,hashmap,prevmap,start,target){
+    let queue=[start]
+    let count=0
+    hashmap[`${start.x}-${start.y}`]=true
+    while (queue.length > 0){
+      count+=1
+      let c=queue[0]
+      queue.shift()
+      refarray[c.x+c.y*50].current.style['transition-delay']=`${count * 8}ms`
+      refarray[c.x+c.y*50].current.classList.add('visited')
+      if (c.x == target.x && c.y == target.y) return [c,count]
+ 
+      
+      
+      if(c.y+1 < 25 && !hashmap[`${c.x}-${c.y+1}`] && !graph[c.y+1][c.x].iswall){
+        queue.unshift({x:c.x ,y:c.y+1})
+        prevmap[`${c.x}-${c.y+1}`]={...c}
+        hashmap[`${c.x}-${c.y+1}`]=true
+      }
+      if(c.x-1 >=0 && !hashmap[`${c.x-1}-${c.y}`] && !graph[c.y][c.x-1].iswall){
+        queue.unshift({x:c.x -1,y:c.y})
+        prevmap[`${c.x-1}-${c.y}`]={...c}
+        hashmap[`${c.x-1}-${c.y}`]=true
+      }
+      if(c.y-1 >=0 && !hashmap[`${c.x}-${c.y-1}`] && !graph[c.y-1][c.x].iswall){
+        queue.unshift({x:c.x ,y:c.y-1})
+        prevmap[`${c.x}-${c.y-1}`]={...c}
+        hashmap[`${c.x}-${c.y-1}`]=true
+      }
+      if(c.x+1 < 50 && !hashmap[`${c.x+1}-${c.y}`] && !graph[c.y][c.x+1].iswall){
+        queue.unshift({x:c.x +1,y:c.y})
+        prevmap[`${c.x+1}-${c.y}`]={...c}
+        hashmap[`${c.x+1}-${c.y}`]=true
+      }
+    }
+    return null
+  }
+
+ useEffect(()=>{
+
+if (algo == 'BFS'){
+  let hashmap={}
+  let prevmap={}
+  for (let j=0;j<25;j++){
+   for (let i=0;i<50;i++){
+     hashmap[`${i}-${j}`]=false
+     prevmap[`${i}-${j}`]=null
+   }
+ }
+ let result=BFS(grid,hashmap,prevmap,start.current,end.current)
+ let path=[]
+ if (result !=null){
+  let current=result[0]
+  while (prevmap[`${current.x}-${current.y}`] != null){
+    path.push(current)
+    current=prevmap[`${current.x}-${current.y}`]
+  }
+  setTimeout(()=>{path.reverse().forEach((elem,index)=>{
+    refarray[elem.x+elem.y*50].current.style['transition-delay']=`${( index) * 15}ms`
+      refarray[elem.x+elem.y*50].current.classList.add('path')
+  })},result[1]*9)
+  
+ }
+  
+ 
+}
+if (algo == 'BDS'){
+  let hashmap={}
+  let prevmap={}
+  for (let j=0;j<25;j++){
+   for (let i=0;i<50;i++){
+     hashmap[`${i}-${j}`]=false
+     prevmap[`${i}-${j}`]=null
+   }
+ }
+  let result=BDS(grid,hashmap,prevmap,start.current,end.current)
+  let path=[]
+  if (result !=null){
+   let current=result[0]
+   while (prevmap[`${current.x}-${current.y}`] != null){
+     path.push(current)
+     current=prevmap[`${current.x}-${current.y}`]
+   }
+   setTimeout(()=>{path.reverse().forEach((elem,index)=>{
+     refarray[elem.x+elem.y*50].current.style['transition-delay']=`${( index) * 15}ms`
+       refarray[elem.x+elem.y*50].current.classList.add('path')
+   })},result[1]*9)
+   
+  }
+   
+  
+ }
+ },[run])
+
+ useEffect(()=>{
+  refarray.forEach((elem)=>{elem.current.style['transition-delay']='0ms'})
+  refarray.forEach((elem)=>{elem.current.classList.remove('visited');elem.current.classList.remove('path')})
+ },[res])
 
   return (
     <div className='board'>
@@ -68,13 +202,13 @@ export default function Grid() {
                break;
 
              case 'addbricks':
-                newgrid=grid.slice()
+                 newgrid=grid.slice()
                newgrid[yindex][xindex]={...newgrid[yindex][xindex],weight:1,iswall:true}
                setgrid(newgrid)
                break;
 
             case 'addweight':
-                newgrid=grid.slice()
+                 newgrid=grid.slice()
                newgrid[yindex][xindex]={...newgrid[yindex][xindex],weight:5,iswall:false}
                setgrid(newgrid)
                break;
